070476afeaa16a601bec7b8e4e25a8a5e6435faf
Sudarshana Tamuly <47175592+sudarshana12@users.noreply.github.com>
2021-10-28 23:17:51 +0530

    updated

--- index.js
+++ index.js
@@ -30,10 +30,18 @@ function convertDateToStr(date){
 function getAllDateFormats(date){
     var dateStr = convertDateToStr(date);
 
-    var ddmmyyyy
-    var mmddyyyy
-    var yyyymmdd
-    var ddmmyy
-    var mmddyy
-    var yymmdd
+    var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
 }
+var date = {
+    day: 2,
+    month:12,
+    year:2021
+}
+console.log(getAllDateFormats(date));
